eadcb32d1cd18ce4c17dfa679d613886da960243
Softech Smart Solutions <144152304+SoftechDevelopers@users.noreply.github.com>
2025-06-14 13:42:27 +0530

    Config file modified for production

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
-const path = require('path')
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
-
-// Remove this if you're not using Fullcalendar features
-
-module.exports = {
+const nextConfig = {
+  output: 'export', // ✅ Enable static export
   trailingSlash: true,
   reactStrictMode: false,
+
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
       apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
+    };
 
-    return config
+    return config;
   }
-}
+};
+
+module.exports = nextConfig;
